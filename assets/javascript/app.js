d8df92819b506423973689c02cca3401d4725512
AlexFP <florianprescott@googlemail.com>
2018-01-25 17:11:48 -0500

    firebase google authorization

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,15 +9,37 @@
 	    messagingSenderId: "53248747928"
 	  },
 
+	  uiConfig: {
+    	signInSuccessUrl: '<url-to-redirect-to-on-success>',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl: '<your-tos-url>'
+    },
+
 	  get ref() {
 
 	  	return firebase.database().ref("trains");
 
 	  },
 
+	  signIn: function() {
+    
+	    // Initialize the FirebaseUI Widget using Firebase.
+	    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+	    // The start method will wait until the DOM is loaded.
+	    ui.start("#firebaseui-auth-container", TS.uiConfig);
+
+	  },
+
 	  ready: function() {
 
 	  	firebase.initializeApp(TS.config);
+
+	  	TS.signIn();
 
 	 		TS.dataListen();
 
